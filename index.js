8f071cc0182a8504220818690f8320e6f78a2cad
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-18 04:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(musicans, instruments){
+  var array = []
+  var string = ""
+  for (var i = 0; i <  musicans.length; i++){
+    string = musicans[i] + " plays " + instruments[i]
+    array.push(string)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  var string = ""
+  var array = []
+  while (i < facts.length){
+    string = facts[i] + "!!!"
+    array.push(string)
+    i++
+  }
+  return array
+}
